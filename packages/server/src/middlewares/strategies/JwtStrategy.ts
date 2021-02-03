874e15e0187b6b3cc9155ccf654340d013b86430
luca <beccibrian@gmail.com>
2021-02-03 18:51:53 -0300

    chore(@package/server ): Jwt strategy and configuration added

---
+++ packages/server/src/middlewares/strategies/JwtStrategy.ts
@@ -0,0 +1,30 @@
+import { ExtractJwt, StrategyOptions, Strategy } from "passport-jwt"
+import { User } from "src/entities/User"
+
+class JwtStrategy{
+    private _options: StrategyOptions
+    public _strategy: Strategy
+
+    constructor(){
+        this._options ={
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_KEY,
+        }
+        this._strategy = new Strategy(this._options, async(payload, done) => {
+            try{
+                const user = await User.findOne({where:{id: payload.id}})
+                if(user){
+                    return done(null, user.id)
+                }
+                return done(null, false)
+            }
+            catch(e){
+                console.log(e)
+            }
+        })
+    }
+}
+
+const jwtStrategy = new JwtStrategy()
+
+export default jwtStrategy._strategy
